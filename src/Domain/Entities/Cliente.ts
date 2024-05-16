95fd0a40f1bf03e74815c89357986c9eb56126bd
jabez <jabez_t.i@hotmail.com>
2024-05-16 15:08:25 -0300

    ajuste apos merge

--- src/Domain/Entities/Cliente.ts
+++ src/Domain/Entities/Cliente.ts
@@ -1,26 +1,12 @@
 export class Cliente {
-<<<<<<< HEAD
 	id: number;
 	nome: string;
 	sobrenome: string | null;
 	cpf: string;
-
 	constructor({ id, cpf, nome, sobrenome }: typeof Cliente.prototype) {
 		this.id = id
 		this.nome = nome;
 		this.sobrenome = sobrenome;
 		this.cpf = cpf;
 	}
-=======
-  id?: number;
-  nome: string;
-  sobrenome: string | null;
-  cpf: string;
-
-  constructor({ cpf, nome, sobrenome }: typeof Cliente.prototype) {
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-    this.cpf = cpf;
-  }
->>>>>>> 8cd1c57b4e84a24e88f4faa7620f3fb5ca521910
 }
