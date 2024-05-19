2464a3e0d221616e94f5a996b1edfab8e696152f
Leonardo Reis <leoeareis@gmail.com>
2024-05-19 09:51:37 +0200

    Adiciona teste unitario para use case de atualizar pedido

---
+++ tests/core/application/use-cases/pedido/AtualizarPedidoUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { PedidoTestRepository } from "@/Interfaces/ExternalServices/Microservices/TestsRepositories/PedidoTestRepository";
+import { beforeEach, describe, expect, it } from "vitest";
+import PedidoGateway from "@/Interfaces/Gataways/PedidoGateway";
+import { StatusPagamento } from "@/Domain/Enums/StatusPagamento";
+import { AtualizarPedidoUseCase } from "@/Application/use-cases/pedidos/AtualizarPedidoUseCase";
+import { Pedido } from "@/Domain/Entities/Pedido";
+
+let useCase: AtualizarPedidoUseCase;
+let pedidoGateway: PedidoGateway;
+
+describe("CriaPedido use case", () => {
+  beforeEach(() => {
+    const pedidoRepository = new PedidoTestRepository();
+    pedidoGateway = new PedidoGateway(pedidoRepository);
+    useCase = new AtualizarPedidoUseCase(pedidoGateway);
+  });
+
+  it("Deve permitir atualizar pedido", async () => {
+    const pedido: Pedido = {
+        id: 1,
+        id_cliente: 1,
+        valor_final: 1,
+        tipo_pagamento: "",
+        status: "",
+        status_pagamento: "",
+    };
+    await pedidoGateway.createAsync(pedido);
+
+    const pedidoRetornado = await useCase.executarAsync(
+        1, 2, "pix", "Em preparacao"
+    )
+    ;
+
+    expect(pedidoRetornado).toBeDefined;
+  });
+
+  it("Não deve permitir atualizar pedido para pedido nao existente", async () => {
+    await expect(useCase.executarAsync(2, null,null,null)).rejects.toThrow(
+      "Pedido com ID 2 não encontrado."
+    );
+  });
+});
