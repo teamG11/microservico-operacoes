1a4e88622e3f471066846346e88e41e6cfd211d8
jabez <jabez_t.i@hotmail.com>
2024-05-19 01:57:06 -0300

    tests: ajuste covarage

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test:{
-    coverage:{
-      exclude:['./tests']
+  test: {
+    coverage: {
+      exclude: [
+        '**/*.spec.ts',    // Exclui especificamente arquivos .spec.ts
+        '**/tests/**',     // Exclui qualquer coisa dentro de uma pasta tests
+        '**/__tests__/**'  // Exclui qualquer coisa dentro de uma pasta __tests__
+      ]
     }
   }
 });
